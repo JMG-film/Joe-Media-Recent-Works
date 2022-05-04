baff00c06123ab40e787063346c9b8ed41ce314b
Blake Frondall (JoeMediaGroup) <blakef@joemedia.tv>
2022-05-04 16:14:57 -0600

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// console.log("bonjour");
+
+// // var x = document.title;
+// // 	console.log(x);
+
+// var x = $("#flex-container");
+// var elHeight = x.outerHeight();
+// var elWidth = x.outerWidth();
+
+// var $wrapper = $("#scaleable-wrapper");
+
+// function doResize(event, ui) {
+
+// 	var scale, origin; 
+
+// 	scale = Math.min(
+// 		ui.size.width / elWidth, 
+// 		ui.size.height / elHeight
+// 	);
+
+// x.css({
+// 	transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+// 	});
+// }
+
+// x.resize({
+// 	resize: doResize
+// });
+
+// var starterData = {
+// 	size: {
+// 		width: $wrapper.width(), 
+// 		height: $wrapper.height()
+// 	}
+// }
+
+// doResize(null, starterData);
